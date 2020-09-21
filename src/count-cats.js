8e9c4308f8283aad5ac5427221cca7a132aeccd2
lesor9 <lesor@protonmail.com>
2020-09-21 10:10:38 +0300

    implement first task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {  	
+  	let cats = 0;
+
+	for (let i = 0; i <= backyard.length - 1; i++) {
+		arr = backyard[i];
+		for (let j = 0; j <= arr.length - 1; j++) {
+			if (backyard[i][j] == '^^') {
+				cats += 1;
+			}
+		}
+	}
+	return cats;
 };
--- test/count-cats.test.js
+++ test/count-cats.test.js
@@ -80,7 +80,7 @@ describe('Count cats!', () => {
         [2, null, 0, 1, ".", "dasdas", true, NaN, "", false, "^^", "^^"],
         [false, ".", 1, 0, "^^", null, "", 2, "dasdas", "^^", NaN, true],
         [".", false, 1, null, NaN, 2, 0, "dasdas", true, "^^", "", "^^"],
-        [false, ".", 1, 0, "^^", true, null, "^^", "", NaN, 2, "dasdas"],
+        ['', false, ".", 1, 0, "^^", true, null, "^^", "", NaN, 2, "dasdas"],
         [false, NaN, 1, 0, ".", "^^", null, true, "dasdas", "^^", 2, ""],
         [null, 1, NaN, true, ".", "^^", "^^", 2, "", false, "dasdas", 0],
         [null, NaN, "", false, ".", 1, 0, "^^", "dasdas", true, 2, "^^"],
@@ -99,7 +99,7 @@ describe('Count cats!', () => {
         [],
       ]), 8);
     });
-
+    
     it.optional('level 3', () => {
       const stringSet = ['aa', '##', false, NaN, 2, 3, '^ ^', undefined, 54, ' ^^'];
       for (let i = 3; i < 60; i++) {
@@ -107,6 +107,7 @@ describe('Count cats!', () => {
         assert.equal(countCats(backyard), count);
       }
     });
+    
   });
 });
 
