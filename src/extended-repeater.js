ede4d150a8cc1c73b8af7bf27ef6ad1a08599246
lesor9 <lesor@protonmail.com>
2020-09-21 23:23:42 +0300

    implement a few more tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	let finalStr = '';
+
+	if (options.repeatTimes === undefined) {
+		return str + options.addition;
+	}
+
+	for (let i = 0; i < options.repeatTimes; i++) {
+
+		finalStr += str;
+
+		if (options.separator === undefined) options.separator = "+";
+		if (options.addition === undefined) options.addition = "";
+		
+
+		addStr = options.addition + options.additionSeparator;
+		addStr = addStr.repeat(options.additionRepeatTimes - 1);
+		addStr += options.addition
+
+		finalStr += addStr;
+		
+		if (i != options.repeatTimes - 1) {
+			finalStr += options.separator;
+		}
+
+	}	
+
+	return finalStr; 
 };
-  
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+	let month = 0;
+	
+	if (date === undefined) return 'Unable to determine the time of year!';
+	if (typeof date != 'object') throw Error;
+
+	if (date instanceof Date) {
+		month = date.getMonth() + 1;
+	}
+	
+	switch (month) {
+		case 12:
+		case 1:
+		case 2:
+			return 'winter';
+
+		case 3:
+		case 4:
+		case 5:
+			return 'spring';
+
+		case 6:
+		case 7:
+		case 8:
+			return 'summer';
+
+		case 9:
+		case 10:
+		case 11:
+			return 'fall';
+	}	
 };
