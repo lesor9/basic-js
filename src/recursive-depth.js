624759f88dc8b2633ffe688d511f4183b3d3f42d
lesor9 <lesor@protonmail.coml>
2020-09-25 16:47:10 +0300

    solved recursive-depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+  	let maxDeap = 0;
+  	for (let i = 0; i < arr.length; i++) {
+	  	const typeArr = Object.prototype.toString.call(arr[i])  === '[object Array]';
+	  	let tempMaxDeap = 0;
+
+  		if (i === arr.length - 1) tempMaxDeap = 1;
+		if (typeArr) tempMaxDeap = (1 + this.calculateDepth(arr[i]));
+		if (typeArr && arr[i].length == 0) tempMaxDeap = 2;
+		if (tempMaxDeap > maxDeap) maxDeap = tempMaxDeap;
+  	}
+
+
+  	return maxDeap;
+
   }
 };
--- test.js
+++ test.js
@@ -7,5 +7,6 @@
 //require('./test/simple-chain.test');
 //require('./test/transform-array.test');
 
+require('./test/recursive-depth.test');
+
 //require('./test/vigenere-cipher.test');
-//require('./test/recursive-depth.test');
