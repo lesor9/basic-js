69b09d3bb0b06570a9cea4b675679db86ac7be16
lesor9 <lesor@protonmail.coml>
2020-09-26 00:00:23 +0300

    almost solved task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,89 +1,90 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt(string, key) {
-  	let j = 0;
-  	let keyLength = key.length;
-  	let newKey = '';
-
-  	string = string.toLowerCase();
-  	key = key.toLowerCase();
-
-  	for (let i = 0; i < string.length; i++) {
-   		if (string.charCodeAt(i) < 97 || string.charCodeAt(i) > 122) {
-  			newKey += ' ';
-  			continue;
-  		}
-  		newKey += key[j];
-  		if (j == keyLength - 1) {
-  			j = 0;
-  			continue;
-  		}
-  		j++;
-  	}
-
-  	let newStr = '';
-  	for (let i = 0; i < string.length; i++) {
-  		const codeStr = string.charCodeAt(i); 
-  		const codeKey = newKey.charCodeAt(i);
-
-  		const finalKey = ( (codeStr + codeKey - 97) > 122) ? (codeStr + codeKey - 97 - 26) : (codeStr + codeKey - 97);
-
-  		if (string.charCodeAt(i) < 94 || string.charCodeAt(i) > 122) {
-  			newStr += string[i];
-  			continue;	
-  		} 
-  		newStr += String.fromCharCode(finalKey);
-  	}
-  	return newStr.toUpperCase();
 
 
-  }    
-  decrypt(encryptedMessage, key) {
-  	let j = 0;
-  	let keyLength = key.length;
-  	let newKey = '';
+	encrypt(string, key) {
+	  	let j = 0;
+	  	let keyLength = key.length;
+	  	let newKey = '';
 
-  	encryptedMessage = encryptedMessage.toLowerCase();
-  	key = key.toLowerCase();
+	  	string = string.toLowerCase();
+	  	key = key.toLowerCase();
 
-  	for (let i = 0; i < encryptedMessage.length; i++) {
-   		if (encryptedMessage.charCodeAt(i) < 97 || encryptedMessage.charCodeAt(i) > 122) {
-  			newKey += ' ';
-  			continue;
-  		}
-  		newKey += key[j];
-  		if (j == keyLength - 1) {
-  			j = 0;
-  			continue;
-  		}
-  		j++;
-  	}
+	  	for (let i = 0; i < string.length; i++) {
+	   		if (string.charCodeAt(i) < 97 || string.charCodeAt(i) > 122) {
+	  			newKey += ' ';
+	  			continue;
+	  		}
+	  		newKey += key[j];
+	  		if (j == keyLength - 1) {
+	  			j = 0;
+	  			continue;
+	  		}
+	  		j++;
+	  	}
 
-  	let newStr = '';
-  	for (let i = 0; i < encryptedMessage.length; i++) {
-  		const codeStr = encryptedMessage.charCodeAt(i); 
-  		const codeKey = newKey.charCodeAt(i);
+	  	let newStr = '';
+	  	for (let i = 0; i < string.length; i++) {
+	  		const codeStr = string.charCodeAt(i); 
+	  		const codeKey = newKey.charCodeAt(i);
 
-		let finalKey = undefined;
+	  		const finalKey = ( (codeStr + codeKey - 97) > 122) ? (codeStr + codeKey - 97 - 26) : (codeStr + codeKey - 97);
 
-		if ( (codeStr - codeKey + 97) > 122) {
-			finalKey = codeStr - codeKey + 97 - 26;
-		} else if ( (codeStr - codeKey + 97) < 97) {
-			finalKey = codeStr - codeKey + 97 + 26;
-		} else {
-			finalKey = codeStr - codeKey + 97;
-		}
+	  		if (string.charCodeAt(i) < 97 || string.charCodeAt(i) > 122) {
+	  			newStr += string[i];
+	  			continue;	
+	  		} 
+	  		newStr += String.fromCharCode(finalKey);
+	  	}
+	  	return newStr.toUpperCase();
+}    
+	decrypt(encryptedMessage, key) {
+	  	let j = 0;
+	  	let keyLength = key.length;
+	  	let newKey = '';
 
-  		if (encryptedMessage.charCodeAt(i) < 97 || encryptedMessage.charCodeAt(i) > 122) {
-  			newStr += encryptedMessage[i];
-  			continue;	
-  		} 
+	  	encryptedMessage = encryptedMessage.toLowerCase();
+	  	key = key.toLowerCase();
 
-  		newStr += String.fromCharCode(finalKey);
-  	}
-  	return newStr.toUpperCase();
-  }
+	  	for (let i = 0; i < encryptedMessage.length; i++) {
+	   		if (encryptedMessage.charCodeAt(i) < 97 || encryptedMessage.charCodeAt(i) > 122) {
+	  			newKey += ' ';
+	  			continue;
+	  		}
+	  		newKey += key[j];
+	  		if (j == keyLength - 1) {
+	  			j = 0;
+	  			continue;
+	  		}
+	  		j++;
+	  	}
+
+	  	let newStr = '';
+	  	for (let i = 0; i < encryptedMessage.length; i++) {
+	  		const codeStr = encryptedMessage.charCodeAt(i); 
+	  		const codeKey = newKey.charCodeAt(i);
+
+			let finalKey = undefined;
+
+			if ( (codeStr - codeKey + 97) > 122) {
+				finalKey = codeStr - codeKey + 97 - 26;
+			} else if ( (codeStr - codeKey + 97) < 97) {
+				finalKey = codeStr - codeKey + 97 + 26;
+			} else {
+				finalKey = codeStr - codeKey + 97;
+			}
+
+	  		if (encryptedMessage.charCodeAt(i) < 97 || encryptedMessage.charCodeAt(i) > 122) {
+	  			newStr += encryptedMessage[i];
+	  			continue;	
+	  		} 
+
+	  		newStr += String.fromCharCode(finalKey);
+	  	}
+	  	return newStr.toUpperCase();
+}
+
 }
 
 module.exports = VigenereCipheringMachine;
